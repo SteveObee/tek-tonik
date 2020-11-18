b6a7b2026c8c23e4f54c5ef554467abf67b5d224
SteveObee <stephenobee1@gmail.com>
2020-11-18 12:21:34 +0000

    Deploy 1811201221

--- post_deploy.sh
+++ post_deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-php artisan migrate:fresh --seed &&
-ln -s /app/storage/app/public /app/public/storage
+ln -s /app/storage/app/public /app/public/storage &&
+php artisan migrate:fresh --seed
